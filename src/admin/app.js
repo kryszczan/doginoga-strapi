6c437db1ef70f9cc9dba37f8a8c0a56b7895c780
Krystian <k.wachnicki@zensite.pl>
2024-07-11 13:57:25 +0700

    Added custom logo text

--- src/admin/app.js
+++ src/admin/app.js
@@ -1,5 +1,15 @@
 const config = {
   locales: ["en", "pl"],
+  translations: {
+    en: {
+      "app.components.LeftMenu.navbrand.title": "Doginoga",
+      "app.components.LeftMenu.navbrand.workplace": "CMS",
+    },
+    pl: {
+      "app.components.LeftMenu.navbrand.title": "Doginoga",
+      "app.components.LeftMenu.navbrand.workplace": "CMS",
+    },
+  },
 };
 
 const bootstrap = (app) => {
